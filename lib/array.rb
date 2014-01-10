324a4e639f4d8d0455202237c9c69610e46f6f35
Doug Teixeira and Timothy Campbell <doug and tim@appacademy.io>
2014-01-10 13:34:03 -0500

    Adds #my_transpose and #stock_picker and specs for both.

--- lib/array.rb
+++ lib/array.rb
@@ -20,4 +20,33 @@ class Array
 
     arr
   end
+
+  def my_transpose
+    arr = Array.new(self.count) {[]}
+
+    self.each_with_index do |row, i|
+      row.each_with_index do |cell, j|
+        arr[j][i] = cell
+      end
+    end
+
+    arr
+  end
+
+  def stock_picker
+    arr = []
+    best_profit = 0
+
+    self.each_index do |i|
+      (i+1..self.count - 1).each do |j|
+        profit = self[j] - self[i]
+        if profit > best_profit
+          arr = [i, j]
+          best_profit = profit
+        end
+      end
+    end
+
+    arr
+  end
 end
--- spec/array_spec.rb
+++ spec/array_spec.rb
@@ -13,4 +13,28 @@ describe Array do
       expect([-1, 0, 2, -2, 1].two_sum).to eq([[0, 4], [2, 3]])
     end
   end
+
+  describe "#my_transpose" do
+    it "transposes an array of arrays" do
+      rows = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8]
+  ]
+
+      cols = [
+          [0, 3, 6],
+          [1, 4, 7],
+          [2, 5, 8]
+        ]
+      expect(rows.my_transpose).to eq(cols)
+    end
+  end
+
+  describe "#stock_picker" do
+    it "picks the most profitable days" do
+      stocks = [3, 0, 2, 9, 3, 4]
+      expect(stocks.stock_picker).to eq([1,3])
+    end
+  end
 end
